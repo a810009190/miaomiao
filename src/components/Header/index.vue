7b711ec1f53ef9c472fbfe45aa421152d938b99e
a810009190 <810009190@qq.com>
2020-08-02 09:27:37 +0800

    add detailPage

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,6 +1,6 @@
 <template>
     <header id="header">
-        <h1>{{ title }}</h1>
+        <slot></slot><h1>{{ title }}</h1>
     </header>
 </template>
 
@@ -19,5 +19,5 @@ export default {
 <style>
 #header{ width: 100%; height: 50px; color: #fff;background: #e54847;border-bottom: 1px solid #e54847; position: relative;}
 #header h1{ font-size: 18px; text-align: center; line-height: 50px;font-weight: normal;}
-#header i{ position: absolute; left: 5px; top: 50%;margin-top: -13;font-size: 26px;}
+#header i{ position: absolute; left: 5px; top: 30%;margin-top: -13;font-size: 26px;}
 </style>
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -20,9 +20,9 @@
             <ul>
                 <li class="pullDown">{{pullDownMsg}}</li>
                 <li v-for="item in movieList" :key="item.id">
-                    <div class="pic_show" @tap="handleToDetail"><img :src="item.img | setWH('128.180')"></div>
+                    <div class="pic_show" @tap="handleToDetail(item.id)"><img :src="item.img | setWH('128.180')"></div>
                     <div class="info_list">
-                        <h2>{{item.nm}}<img src="@/assets/maxs.png" v-if="item.version"></h2>
+                        <h2 @tap="handleToDetail(item.id)">{{item.nm}}<img src="@/assets/maxs.png" v-if="item.version"></h2>
                         <p>观众评 <span class="grade">{{item.sc}}</span></p>
                         <p>主演:{{item.star}}</p>
                         <p>{{item.showInfo}}</p>
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+    
 export default {
     name : 'NowPlaying',
     data() {
@@ -91,8 +92,8 @@ export default {
         });
     },
     methods : {
-        handleToDetail() {
-            console.log('showDetail');
+        handleToDetail(movieId) {
+            this.$router.push('/movie/detail/1/' + movieId);
         },
         handleToScroll(pos){
             if( pos.y > 30 ){
