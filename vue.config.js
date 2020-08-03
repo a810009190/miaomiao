3988e5b87e7c684907b60870fb50beca38d0977a
a810009190 <810009190@qq.com>
2020-08-03 15:30:18 +0800

    bug cleat

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+    configureWebpack: {
+
+        // other webpack options to merge in ...
+
+    },
+
+    // devServer Options don't belong into `configureWebpack`
     devServer : {
+        public : '0.0.0.0:8080',
+
+        hot : true,
+
+        disableHostCheck : true,
+        
         proxy : {
             '/api' : {
                 target : 'https://getman.cn/mock',
